087099c02d978bd89bc713b445568f8bd392f8f2
George Harris <citizenharris@gmail.com>
2022-01-17 15:59:48 +0000

    Refactor similar tests

--- mars_rover/test/rover_test.dart
+++ mars_rover/test/rover_test.dart
@@ -5,19 +5,22 @@ import 'package:mars_rover/mars_rover.dart';
 
 void main() {
   group("Mars Rover unit tests", () {
+    late MarsRover rover;
+    setUp(() {
+      rover = MarsRover(Engine(), Compass());
+    });
     test("Rover should initialise facing North", () {
-      var rover = MarsRover(Engine(), Compass());
       expect(rover.move(""), "0:0:N");
     });
 
-    test("Rover should move forward 1 square", () {
-      var rover = MarsRover(Engine(), Compass());
-      expect(rover.move("M"), "0:1:N");
-    });
-
-    test("Rover should move 2 squares forwards", () {
-      var rover = MarsRover(Engine(), Compass());
-      expect(rover.move("MM"), "0:2:N");
+    var inputsToExpected = {
+      "M": "0:1:N",
+      "MM": "0:2:N"
+    };
+    inputsToExpected.forEach((input, expected) {
+      test("Rover can move: given `$input` -> $expected", () {
+        expect(rover.move(input), expected);
+      });
     });
   });
 }
